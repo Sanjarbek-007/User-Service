151db6703541c50af8f75bf9eb92f55c1d40cc36
Sanjarbek <sanjarbekabdurahmonov123@gmail.com>
2024-09-12 21:56:55 +0500

    Ready

--- migrations/000002_create_refresh_token_table.up.sql
+++ migrations/000002_create_refresh_token_table.up.sql
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS users (
 CREATE SERVER server1_fdw FOREIGN DATA WRAPPER postgres_fdw OPTIONS (host '3.75.208.130', port '5432', dbname 'google_docs');
 CREATE USER MAPPING FOR postgres SERVER server1_fdw OPTIONS (user 'postgres', password '1234');
 CREATE FOREIGN TABLE users_server1 (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    id UUID DEFAULT gen_random_uuid(),
     email VARCHAR(50) UNIQUE NOT NULL,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
